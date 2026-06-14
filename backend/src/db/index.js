209d2a784e2e3033aa79e2ef69def39542ed0a25
mukki <mukkiaryan8650@gmail.com>
2026-06-14 15:23:16 +0530

    fix: update MongoDB connection string to include database name

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -3,8 +3,10 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
+    console.log("Connecting to MongoDB...");
+    console.log(`${process.env.MONGODB_URI}/${DB_NAME}`);
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}`
+      `${process.env.MONGODB_URI}/${DB_NAME}`
     );
     console.log(
       `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
